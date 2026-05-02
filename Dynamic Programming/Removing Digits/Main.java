4ee460a11f46b9f06fcc5686172ab46059d7b9e5
Dhanush N <mrcoyote2075@gmail.com>
2026-05-02 17:52:13 +0530

    feat: Solved Removing Digits

--- Dynamic Programming/Removing Digits/Main.java
+++ Dynamic Programming/Removing Digits/Main.java
@@ -17,9 +17,9 @@ public class Main {
             return buffer[ptr++];
         }
 
-        public long nextLong() throws Exception {
+        public int nextInt() throws Exception {
             int sign = 1, c;
-            long val = 0;
+            int val = 0;
 
             do
                 c = read();
@@ -39,7 +39,60 @@ public class Main {
         }
     }
 
+    public static int MAX = (int) 1e9;
+    public static int[] dp;
+
+    public static int rec(int lev) {
+        if (lev == 0)
+            return 0;
+
+        if (dp[lev] != -1)
+            return dp[lev];
+
+        String s = String.valueOf(lev);
+
+        int ans = MAX;
+        for (int i = 0; i < s.length(); i++) {
+            int val = s.charAt(i) - '0';
+            if (0 < val)
+                ans = Math.min(ans, rec(lev - val) + 1);
+        }
+
+        return dp[lev] = ans;
+    }
+
+    public static int iter(int n) {
+        for (int lev = 1; lev <= n; lev++) {
+
+            String s = String.valueOf(lev);
+
+            int ans = MAX;
+            for (int i = 0; i < s.length(); i++) {
+                int val = s.charAt(i) - '0';
+
+                if (0 < val)
+                    ans = Math.min(ans, dp[lev - val] + 1);
+            }
+
+            dp[lev] = ans;
+        }
+
+        return dp[n];
+    }
+
+    public static int solve(int n) {
+        dp = new int[n + 1];
+        // Arrays.fill(dp, -1);
+        // return rec(n);
+
+        return iter(n);
+    }
+
     public static void main(String[] args) throws Exception {
         UltraFastReader in = new UltraFastReader();
+
+        int n = in.nextInt();
+
+        System.out.println(solve(n));
     }
 }
