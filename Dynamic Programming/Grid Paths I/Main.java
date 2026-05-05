744bc1a087f8f3787f90b9f3289e651ee20f5da6
Dhanush N <mrcoyote2075@gmail.com>
2026-05-05 10:15:15 +0530

    feat: Init Grid Paths I

---
+++ Dynamic Programming/Grid Paths I/Main.java
@@ -0,0 +1,65 @@
+
+import java.io.InputStream;
+
+public class Main {
+    public static class UltraFastReader {
+        public final InputStream in = System.in;
+        public final byte[] buffer = new byte[1 << 16];
+        public int ptr = 0, len = 0;
+
+        public int read() throws Exception {
+            if (ptr >= len) {
+                ptr = 0;
+                len = in.read(buffer);
+                if (len <= 0)
+                    return -1;
+            }
+
+            return buffer[ptr++];
+        }
+
+        public int nextInt() throws Exception {
+            int sign = 1, val = 0, c;
+
+            do
+                c = read();
+            while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            while (c > ' ') {
+                val = (val * 10) + (c - '0');
+                c = read();
+            }
+
+            return val * sign;
+        }
+    }
+
+    public static int n;
+    public static int MAX = (int) 1e9;
+    public static int MIN = (int) -1e9;
+    public static long MOD = (long) 1e9 + 7;
+
+    public static int solve(int[] arr, int x) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        UltraFastReader in = new UltraFastReader();
+        n = in.nextInt();
+        int x = in.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(solve(arr, x));
+    }
+
+}
